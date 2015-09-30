91f5dd2c674cc8233beb2b34d79df3d437c16ee1
DannyZurg <DannyZurg@ninja24.d22.lan>
2015-09-30 06:10:16 -0500

    Primes

---
+++ NetBeansProjects/Cruz_3_primes/src/cruz_3_primes/Cruz_3_primes.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cruz_3_primes;
+
+/**
+ *
+ * @author DannyZurg
+ */
+public class Cruz_3_primes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int evens = 0;
+    for(int i = 0; i < 100;i++){
+        if(i % 2 == 0){
+            evens = evens + 1;
+        }    
+            System.out.println(i +" isEven:  isOdd");
+    }        
+    }
+}
+    /*
+    What is a Method
+        A Method is a function 
+    It is like a little program,e.g.
+    Like finding a square root,
+    Something you might want to do again and again.
+    */
+
+       static boolean isEven(int number){
+       return (number % 2 !== 0)}
+
+       static boolean isOdd(int number)
+       return (number % 2 !== 0);
+       
+       static boolean isPrime (int number)
+       boolean prime = true;
+       int divisors = 0;
+       
+       
+       
+        // A return value is the type of value you are giving back to the program
+        
+        
